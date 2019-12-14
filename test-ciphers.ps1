48d65d3e0944be3de87c21a3adcb6f534bcf9722
Jeremy Busk <jeremybusk@gmail.com>
2019-12-14 15:01:26 -0700

    some test examples

--- test-ciphers.ps1
+++ test-ciphers.ps1
@@ -1,3 +1,10 @@
 # [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $r = invoke-webrequest "https://pyrofex.io"
 $r.BaseResponse | findstr ProtocolVersion
+
+# ./curl.exe -v --tlsv1.1 https://google.com | select -first 10
+
+# ./app/curl/curl.exe -v --tlsv1.1 https://google.com
+# curl.exe -v --tlsv1.1 https://google.com
+# nmap -sV --script ssl-enum-ciphers -p 443 <host>
+# nmap -sV --script ssl-enum-ciphers -p 3389 <host>
