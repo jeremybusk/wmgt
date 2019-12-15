eb1babb5aaa678c6fe69ed0fe226dbfa2a881c1f
Jeremy Busk <jeremybusk@gmail.com>
2019-12-14 19:47:19 -0700

    cipher order

--- tls1.2-1.1.ps1
+++ tls1.2-1.1.ps1
@@ -238,8 +238,10 @@ if ([System.Version]$os.Version -lt [System.Version]'10.0') {
   #   'TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA',
   #   'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA'
   # )
+    # 'TLS_RSA_WITH_3DES_EDE_CBC_SHA',
   $cipherSuitesOrder = @(
-    'TLS_RSA_WITH_3DES_EDE_CBC_SHA',
+    'TLS_DHE_RSA_WITH_AES_128_CBC_SHA',
+    'TLS_DHE_RSA_WITH_AES_256_CBC_SHA',
     'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256',
     'TLS_RSA_WITH_AES_128_GCM_SHA256',
     'TLS_RSA_WITH_AES_256_CBC_SHA256',
@@ -260,9 +262,7 @@ if ([System.Version]$os.Version -lt [System.Version]'10.0') {
     'TLS_PSK_WITH_AES_256_GCM_SHA384',
     'TLS_PSK_WITH_AES_128_GCM_SHA256',
     'TLS_PSK_WITH_AES_256_CBC_SHA384',
-    'TLS_PSK_WITH_AES_128_CBC_SHA256',
-    'TLS_DHE_RSA_WITH_AES_128_CBC_SHA',
-    'TLS_DHE_RSA_WITH_AES_256_CBC_SHA'
+    'TLS_PSK_WITH_AES_128_CBC_SHA256'
   )
 }
 $cipherSuitesAsString = [string]::join(',', $cipherSuitesOrder)
