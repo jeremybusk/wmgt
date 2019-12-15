52c2733a9c77290ea5cfb391a8e555fa85aeca08
Jeremy Busk <jeremybusk@gmail.com>
2019-12-14 17:00:17 -0700

    add stop script

---
+++ stop.ps1
@@ -0,0 +1,11 @@
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+
+{
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
+
+taskkill /f /im tshark.exe
+
+# Stop-Process -Name "tshark"
